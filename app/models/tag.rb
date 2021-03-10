d0c52fe1f6102cb054f4161eaab93c3039cccbe7
Cleyton Silva <silva.rcleyton@gmail.com>
2021-03-10 20:10:35 -0300

    Add model tag_content

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
   belongs_to :user
+  has_many :tag_contents
+  has_many :contents, through: :tag_contents
+
+  validates :name, presence: true, uniqueness: true: { scope: :user_id }
 end
