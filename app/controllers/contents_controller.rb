ed731f07aa2a6dc09f24d16ece6aeb6134ce8534
Cleyton Silva <silva.rcleyton@gmail.com>
2021-03-11 11:57:14 -0300

    Add view edit content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -9,4 +9,26 @@ class ContentsController < ApplicationController
     @content = Content.new
   end
 
+  def create
+    @content = current_user.contents.build(content_params)
+    if @content.save
+      redirect_to contents_path, notice: "Conteúdo criado com sucesso!"
+    else  
+      render :new
+    end
+  end
+
+  def edit
+    @content = Content.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+  private
+    def content_params
+      params.require(:content).permit(:title, :description)
+    end
+
 end
